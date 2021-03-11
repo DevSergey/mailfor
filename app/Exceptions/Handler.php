ec9d6044582c8bed129702bbb73b0becfee1f055
Sergey <sergey.developer.it@gmail.com>
2021-03-11 18:05:21 +0000

    Add sentry support

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -12,6 +12,9 @@ class Handler extends ExceptionHandler
     ];
     public function report(Exception $exception)
     {
+        if (app()->bound('sentry') && $this->shouldReport($exception)) {
+            app('sentry')->captureException($exception);
+        }
         parent::report($exception);
     }
     public function render($request, Exception $exception)
